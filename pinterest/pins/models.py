aab26d023fde68e81e71861248437b893f03ae40
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-05-09 15:50:26 +0300

    add link to specific image on the index page

--- pinterest/pins/models.py
+++ pinterest/pins/models.py
@@ -1,14 +1,31 @@
 from django.db import models
+# rom django.core.urlresolvers import reversef
 
 # Create your models here.
 class Location(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']
 class Category(models.Model):
     name = models.CharField(max_length=30)
     
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['name']
+
 class Image(models.Model):
     image = models.ImageField(upload_to = 'images/')
     name = models.CharField(max_length=30)
     location = models.ForeignKey(Location)
     category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.name
+    # def get_absolute_url(self):
+    #     return reverse("pins:details", kwargs={"id": self.id})
+    class Meta:
+        ordering = ['name']
