502bcd606f6333e2e18f431614e1a504ab2d07ec
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-05-09 15:47:57 +0300

    add function to get details of a specific image

--- pinterest/pins/views.py
+++ pinterest/pins/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404,redirect
+from django.http import HttpResponse,Http404
+from . models import Image,Location,Category
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    images= Image.objects.all()
+
+    return render(request, 'index.html',{"images":images})
+
+def details(request, image_id):
+    try:
+        specific = Image.objects.get(id=image_id)
+    except DoesNotExist:
+        raise Http404()
+    
+    return render(request, "details.html",{"specific":specific})
